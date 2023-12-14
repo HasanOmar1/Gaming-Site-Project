033ac3657c18ea4f7b0ec925d19505dd0800e5f2
HasanOmar1 <r3domar1@gmail.com>
2023-12-14 20:23:26 +0200

    added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,6 @@ import { useUserData } from "../../Contexts/UserDataContext/UserDataContext";
 
 export default function Navbar() {
   const { currentUser, setCurrentUser } = useUserData();
-  const navigate = useNavigate();
   // const location = useLocation();
   // if (location.pathname === `/404`) {
   //   return null;
@@ -14,32 +13,61 @@ export default function Navbar() {
   }
 
   return (
-    <div className="Header">
+    <nav className="Header">
       <Link to={"/"} className="logo">
         <div>
           <h1>GAMES</h1>
         </div>
       </Link>
-      {!currentUser ? (
-        <>
-          <Link to={"/login"} className="loginOrRegister login">
-            <div>Log in</div>
-          </Link>
-          <Link to={"/register"} className="loginOrRegister register">
-            <div>Join us</div>
-          </Link>
-        </>
-      ) : (
-        <>
-          <h3 className="logged-user">{currentUser.email}</h3>
-          <div className="my-library ">My Library</div>
-          <Link to={"/"} className="link">
-            <div onClick={handleLogOut} className="loginOrRegister logout">
-              Log out
-            </div>
-          </Link>
-        </>
-      )}
-    </div>
+
+      <div className="navbar-links">
+        <nav id="menu">
+          <input type="checkbox" id="responsive-menu" />
+          <label></label>
+          <ul>
+            <li>
+              <a class="dropdown-arrow">Products</a>
+              <ul class="sub-menus">
+                <li>
+                  <Link to="/">Products 1</Link>
+                </li>
+                <li>
+                  <Link to="/">Products 2</Link>
+                </li>
+                <li>
+                  <Link to="/">Products 3</Link>
+                </li>
+                <li>
+                  <Link to="/">Products 4</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <Link to={"/"} className="link home">
+          Home
+        </Link>
+        {!currentUser ? (
+          <>
+            <Link to={"/login"} className="loginOrRegister login">
+              <div>Log in</div>
+            </Link>
+            <Link to={"/register"} className="loginOrRegister register">
+              <div>Join us</div>
+            </Link>
+          </>
+        ) : (
+          <>
+            <h3 className="logged-user">{currentUser.email}</h3>
+            <div className="my-library ">My Library</div>
+            <Link to={"/"} className="link">
+              <div onClick={handleLogOut} className="loginOrRegister logout">
+                Log out
+              </div>
+            </Link>
+          </>
+        )}
+      </div>
+    </nav>
   );
 }
