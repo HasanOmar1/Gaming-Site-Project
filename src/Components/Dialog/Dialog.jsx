608a47727e0f507bbbc0769f7ab6510a4c839913
HasanOmar1 <r3domar1@gmail.com>
2023-12-19 17:51:24 +0200

    Added

--- src/Components/Dialog/Dialog.jsx
+++ src/Components/Dialog/Dialog.jsx
@@ -13,21 +13,6 @@ const dialogModal = forwardRef(function Dialog({ children }, ref) {
 
   return (
     <dialog ref={ref}>
-      {/* {currentUser && (
-        <>
-          <h3>Added To Library</h3>
-          <form className="dialog-form" method="dialog">
-            <button className="close-dialog">Close</button>
-            <button
-              className="close-dialog"
-              onClick={() => navigate("/library")}
-            >
-              Go To Library
-            </button>
-          </form>
-        </>
-      )} */}
-
       {!currentUser && (
         <>
           <h3>Please Log in First</h3>
