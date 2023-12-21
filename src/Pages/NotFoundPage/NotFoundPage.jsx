0f60b74a8633a75956a0ad33c8eee770622f48a1
HasanOmar1 <r3domar1@gmail.com>
2023-12-21 17:19:32 +0200

    added

--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -1,12 +1,17 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./NotFoundPage.css";
 
 export default function NotFoundPage() {
   const location = useLocation();
-  //   console.log(location);
+  const navigate = useNavigate();
+
+  setTimeout(() => {
+    navigate("/");
+  }, 3000);
   return (
     <main className="NotFoundPage page">
       <h1>ERROR 404 , PAGE NOT FOUND!</h1>
+      <h1>Redirecting You back to the homepage in 3 seconds</h1>
     </main>
   );
 }
